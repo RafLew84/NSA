ad9f9cc27690dfda897ffbd355bf7c3e661b4347
Rafal Lewandkow <r.lewandkow@gmail.com>
2024-08-16 21:36:35 +0200

    Update observable.py

--- data/observer/observable.py
+++ data/observer/observable.py
@@ -17,19 +17,3 @@ class Observable:
     def notify_observers(self, *args, **kwargs):
         for observer in self._observers:
             observer.update(self, *args, **kwargs)
-
-# class Observable:
-#     def __init__(self):
-#         self._observers = []
-
-#     def add_observer(self, observer):
-#         if observer not in self._observers:
-#             self._observers.append(observer)
-
-#     def remove_observer(self, observer):
-#         if observer in self._observers:
-#             self._observers.remove(observer)
-
-#     def notify_observers(self):
-#         for observer in self._observers:
-#             observer.update()
